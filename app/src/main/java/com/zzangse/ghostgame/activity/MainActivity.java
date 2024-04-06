9973c052b0960e0481b14979a832bc4ab115d61c
sehoon <zzangse1014@gmail.com>
2024-04-06 18:02:30 +0900

    ✨ 뒤로가기 버튼 이벤트
    뒤로가기 2번 클릭시 앱 종료

--- app/src/main/java/com/zzangse/ghostgame/activity/MainActivity.java
+++ app/src/main/java/com/zzangse/ghostgame/activity/MainActivity.java
@@ -1,15 +1,22 @@
 package com.zzangse.ghostgame.activity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.widget.Toast;
+import android.window.OnBackInvokedDispatcher;
 
+import androidx.activity.OnBackPressedCallback;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.os.BuildCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.google.android.material.navigation.NavigationBarView;
+import com.google.android.material.snackbar.Snackbar;
 import com.zzangse.ghostgame.R;
 import com.zzangse.ghostgame.databinding.ActivityMainBinding;
 import com.zzangse.ghostgame.fragment.HomeFragment;
@@ -20,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
     private HomeFragment homeFragment;
     private SettingFragment settingFragment;
+    private long backBtnTime  = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,8 +42,43 @@ public class MainActivity extends AppCompatActivity {
         replace(new HomeFragment());
         BottomNavSelect bottomNavSelect = new BottomNavSelect();
         binding.bottomNav.setOnItemSelectedListener(bottomNavSelect);
-
+        getOnBackPressedDispatcher().addCallback(this,onBackPressedCallback);
     }
+
+    // 뒤로가기   API 33 이상 버전
+    private final OnBackPressedCallback onBackPressedCallback = new OnBackPressedCallback(true) {
+        @Override
+        public void handleOnBackPressed() {
+            long curTime = System.currentTimeMillis();
+            long gapTime = curTime - backBtnTime;
+            if (gapTime >= 0 && gapTime <= 2000) {
+                Log.d("Back_BTN","handleOnBackPressed");
+                finish();
+            } else {
+                backBtnTime=curTime;
+                Snackbar.make(binding.fragmentHome,"한번 더 누르면 종료됩니다.",Snackbar.LENGTH_SHORT).show();
+            }
+        }
+    };
+
+
+    // 뒤로가기  API 33 미만 버전
+    @Override
+    public void onBackPressed() {
+
+        long curTime = System.currentTimeMillis();
+        long gapTime = curTime - backBtnTime;
+
+        if (gapTime >= 0 && gapTime <= 2000) {
+            Log.d("Back_BTN","onBackPressed");
+            super.onBackPressed();
+        } else {
+            backBtnTime = curTime;
+            Snackbar.make(binding.fragmentHome, "한번 더 누르면 종료됩니다.", Snackbar.LENGTH_SHORT).show();
+        }
+    }
+
+
 
     class BottomNavSelect implements NavigationBarView.OnItemSelectedListener {
         @Override
