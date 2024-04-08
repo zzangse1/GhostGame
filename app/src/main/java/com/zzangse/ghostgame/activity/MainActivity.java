800a5448b506404b65fe853cc19cf8d695c3e5cc
zzangse <zzangse1014@gmail.com>
2024-04-08 17:57:32 +0900

    🔥 import 삭제

--- app/src/main/java/com/zzangse/ghostgame/activity/MainActivity.java
+++ app/src/main/java/com/zzangse/ghostgame/activity/MainActivity.java
@@ -3,14 +3,10 @@ package com.zzangse.ghostgame.activity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
-import android.widget.Toast;
-import android.window.OnBackInvokedDispatcher;
 
 import androidx.activity.OnBackPressedCallback;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.os.BuildCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
